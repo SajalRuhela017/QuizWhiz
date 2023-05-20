a0cf03c4b3f2bcd0f8d535d44ac1f7ee6b6d9593
SajalRuhela017 <ruhela.sajal017@gmail.com>
2023-05-20 15:49:34 +0530

    Added Login/Signup using Firebase

--- app/src/main/java/com/example/quizwhiz/SignupActivity.kt
+++ app/src/main/java/com/example/quizwhiz/SignupActivity.kt
@@ -2,10 +2,49 @@ package com.example.quizwhiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.example.quizwhiz.databinding.ActivitySignupBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class SignupActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignupBinding
+    lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signup)
+        binding = ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        firebaseAuth = FirebaseAuth.getInstance()       //created an instance of Firebase Auth
+        binding.btnSignup.setOnClickListener {
+            signUpUser()
+        }
+    }
+
+    private fun signUpUser() {
+        val email = binding.etEmailAddress.text.toString()
+        val password = binding.etPassword.text.toString()
+        val confirmPassword = binding.etConfirmPassword.text.toString()
+
+        if(email.isBlank() || password.isBlank() || confirmPassword.isBlank()) {
+            Toast.makeText(this, "Please enter all credentials", Toast.LENGTH_SHORT).show()
+            return
+        }
+        if(password != confirmPassword) {
+            Toast.makeText(this, "Password does not match", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        firebaseAuth.createUserWithEmailAndPassword(email, password)        //created a user with email and password
+            .addOnCompleteListener(this) {
+                if(it.isSuccessful){
+                    //call successful
+                    Toast.makeText(this, "Signup successful", Toast.LENGTH_SHORT).show()
+                }
+                else {
+                    //call unsuccessful
+                    Toast.makeText(this, "Some error occurred", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
